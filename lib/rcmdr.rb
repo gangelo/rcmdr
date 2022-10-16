6a70537d31d62a1198e59277c82ed63ec85fb249
gangelo <web.gma@gmail.com>
2022-10-16 15:10:36 -0400

    Add requires for this gem to the main .rb

--- lib/rcmdr.rb
+++ lib/rcmdr.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
+require_relative 'rcmdr/command_help'
+require_relative 'rcmdr/command'
+require_relative 'rcmdr/subcommand'
 require_relative 'rcmdr/version'
 
 module Rcmdr
